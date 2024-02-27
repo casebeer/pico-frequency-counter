7c0fa85d103a40cea326cea31b1dd51773fe56ba
Christopher H. Casebeer <christopher@chc.name>
2024-02-27 17:19:30 -0500

    Add documentation to constants

--- constants.py
+++ constants.py
@@ -12,7 +12,7 @@ PIO_FREQ = 125_000_000
 # (9998082./9998500)**-1 = 1.000041808018778
 CORRECTION = 1.00004
 GATE_CYCLES = PIO_FREQ // 10  # i.e. 100 ms gate time
-MAX_COUNT = (1 << 32) - 1
+MAX_COUNT = (1 << 32) - 1  # i.e. 0xffff ffff
 
 COUNTER_INPUT_PIN = 10
 # Counter input pin must be the next pin after the gate pin,
@@ -21,6 +21,6 @@ COUNTER_GATE_PIN = COUNTER_INPUT_PIN - 1
 COUNTER_PULSE_FIN_PIN = 8
 
 # Program behavior
-IDLE_THRESHOLD_MS = 3000 # stop displaying last freq after delay w/no detected freq
+IDLE_THRESHOLD_MS = 3000  # stop displaying last freq after delay w/no detected freq
 INIT_IDLE_THRESHOLD_MS = 1000  # shorter idle time when booting up
-IDLE_SLEEP_MS = 100 # time to sleep when no freq detected
+IDLE_SLEEP_MS = 100  # time to sleep between loops when no freq detected
